7251e30a525d0b6d987f547d4806891d6db625ea
李凯涛 <kt@student-10-201-35-148.wireless.adelaide.edu.au>
2023-03-15 14:15:37 +1030

    for branch2

--- instance.sh
+++ instance.sh
@@ -6,12 +6,3 @@ word=$2
 
 # Display the text Instances of [WORD] in [FILE]
 echo "Instances of $word in $file:"
-
-# Display the first 3 occurrences of [WORD]
-echo "First 3 occurrences:"
-grep -m3 $word $file
-echo ""
-
-# Display the last 2 occurrences of [WORD]
-echo "Last 2 occurrences:"
-grep -m2 $word $file | tail -n2
